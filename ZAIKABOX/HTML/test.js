1542dcfcb75e89244ef087ec621193c62b79d3ea
Pranay <pranaysuthar639@gmail.com>
2025-10-02 01:15:09 +0530

    Problem Solving Attempt #2

--- ZAIKABOX/HTML/test.js
+++
@@ -1,6 +0,0 @@
-// Test file to verify JS deployment and modules
-console.log('JS files are working!');
-
-// Test ES6 module import
-import { auth } from './JS/firebase-config.js';
-console.log('Firebase config loaded:', auth ? 'Success' : 'Failed');
